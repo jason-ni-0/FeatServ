1cb46c0bb8e6ca3bac570148ecb1e54514a96ac3
Jason Ni <jasonni@ucsb.edu>
2022-08-25 14:22:37 -0700

    lat and long

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.get('/results', function(req, res){
       categories: req.query.categories,
       price: req.query.price,
       limit: '50',
-      latitude: 34.59,
-      longitude: -120.0
+      latitude: req.query.latitude,
+      longitude: req.query.longitude
     }
   };
   
